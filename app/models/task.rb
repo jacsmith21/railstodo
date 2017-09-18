c2679c93b37fd620eea9a31b1309bc5169104362
Jacob Smith <jacob.smith@unb.ca>
2017-09-18 16:15:40 -0300

    added some validation, refactored code a bit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+  validates_presence_of :title
+  validate :future_completed_date
+
+  private
+  def future_completed_date
+    if !completed.blank? &amp;&amp; completed &gt; Date.today
+      self.errors.add(:completed, "can't be in the future")
+    end
 end
